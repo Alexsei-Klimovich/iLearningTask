2c601ec29e5c193badd42794dcdd44b42ac94d3f
alexsei <repyls2@mail.ru>
2022-02-16 00:46:08 +0300

    add UserController methods

--- src/main/java/ru/alexsei/task3/controller/MainController.java
+++ src/main/java/ru/alexsei/task3/controller/MainController.java
@@ -1,26 +1,34 @@
 package ru.alexsei.task3.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.alexsei.task3.model.MyUserDetails;
+import ru.alexsei.task3.model.User;
+import ru.alexsei.task3.service.MyUserDetailsService;
+import ru.alexsei.task3.util.ValidationUtil;
 
 import java.security.Principal;
+import java.util.Date;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    MyUserDetailsService userDetailsService;
 
     @GetMapping("/")
     public String welcomePage(Model model) {
         return "welcomePage";
     }
 
-    @GetMapping("/users")
-    public String usersForm(Model model) {
-        return "usersForm";
-    }
-
     @GetMapping("/registration")
-    public String registrationPage(Model model){
+    public String registrationPage(Model model, @ModelAttribute User user){
+        model.addAttribute("user", user);
         return "registrationPage";
     }
 
--- src/main/java/ru/alexsei/task3/service/MyUserDetailsService.java
+++ src/main/java/ru/alexsei/task3/service/MyUserDetailsService.java
@@ -1,6 +1,15 @@
 package ru.alexsei.task3.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.orm.hibernate5.SpringSessionContext;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,10 +18,11 @@ import ru.alexsei.task3.model.MyUserDetails;
 import ru.alexsei.task3.model.User;
 import ru.alexsei.task3.repository.UserRepository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 @Service
-public class MyUserDetailsService implements UserDetailsService {
+public class MyUserDetailsService implements UserDetailsService, ApplicationListener<AuthenticationSuccessEvent> {
 
     @Autowired
     UserRepository userRepository;
@@ -40,8 +50,66 @@ public class MyUserDetailsService implements UserDetailsService {
         }
         return false;
     }
+    public  boolean isEmailExists(String email){
+        if (!userRepository.findByEmail(email).isPresent()){
+            return false;
+        }
+        return true;
+    }
+    public boolean isUserNameExists(String email) {
+        if (!userRepository.findByUserName(email).isPresent()){
+            return false;
+        }
+        return true;
+    }
 
     public void createUser(User user){
         userRepository.save(user);
     }
+
+    //updating last activity
+    @Override
+    public void onApplicationEvent(AuthenticationSuccessEvent event) {
+        String userName = ((UserDetails) event.getAuthentication().
+                getPrincipal()).getUsername();
+        User user = userRepository.findByUserName(userName).get();
+        updateLastActivity(user);
+    }
+
+    public void updateLastActivity(User user){
+        user.setLastActivity(new Date());
+        userRepository.deleteById(user.getId());
+        userRepository.save(user);
+    }
+
+    public void lockUserById(Long id){
+        User newUser = userRepository.getById(id);
+        userRepository.deleteById(id);
+        newUser.setEnabled(false);
+        userRepository.save(newUser);
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (newUser.getUserName().equals(auth.getName())) {
+            auth.setAuthenticated(false);
+        }
+    }
+
+    public void deleteUserById(Long id){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = getUserById(id);
+        deleteUser(id);
+        if (user.getUserName().equals(auth.getName())) {
+            auth.setAuthenticated(false);
+        }
+    }
+
+    public void unlockUserById(Long id){
+        User newUser = userRepository.getById(id);
+        userRepository.deleteById(id);
+        newUser.setEnabled(true);
+        userRepository.save(newUser);
+    }
+
+
+
 }
